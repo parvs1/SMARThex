bcff5640f931daae2e38962306dd23c043736889
pdant <pdantu1234@gmail.com>
2019-02-01 12:01:37 -0500

    initial ble connection java code

--- app/src/main/java/com/example/shrey/medremindertest/MainActivity.java
+++ app/src/main/java/com/example/shrey/medremindertest/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.shrey.medremindertest;
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +17,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -39,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     FloatingActionButton addMedicine; //floating action button on MainActivity
     public final int REQUEST_CODE = 4; //code for starting editMedicine Activity and obtaining its result
     public final String TAG = "com.med-adherence"; //TAG for log usage
+    private BluetoothAdapter bleadapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,126 +53,129 @@ public class MainActivity extends AppCompatActivity {
         String medicinesFileText = "";
         FileInputStream fileInputStream = null;
 
-        //try finding file 'medicinesFile.txt' if it exists
-        try {
-            fileInputStream = openFileInput("medicinesFile.txt");
-            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+        // Use this check to determine whether BLE is supported on the device.
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, "Bluetooth not available", Toast.LENGTH_SHORT).show();
 
-            char[] buffer = new char[1024];
-            int charRead;
+            // initialize the bluetooth adapter
+            final BluetoothManager bleManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+            bleadapter = bleManager.getAdapter();
 
-            //creates string that contains text from 'medicinesFile.txt'
-            while ((charRead=inputStreamReader.read(buffer))>0)
-            {
-                String readString = String.copyValueOf(buffer,0,charRead);
-                medicinesFileText+=readString;
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        String[] medicinesFileTextArray = medicinesFileText.split("\n"); //split text from 'medicinesFile.txt' by line
-
-        medicines = new ArrayList<Medicine>();
-
-        //if medicinesFileTextArray contains a medicine (min length for a medicine is 4); this only triggers if user has not created a medicine yet
-        if(medicinesFileTextArray.length > 2) {
-
-            //iterate line by line to create the arraylist of medicines
-            for (int i = 0; i < medicinesFileTextArray.length; i+=4){
-                String tempName = medicinesFileTextArray[i];
-                String tempHour = medicinesFileTextArray[i+1];
-                String tempMin = medicinesFileTextArray[i+2];
-                int tempFreq = Integer.parseInt(medicinesFileTextArray[i+3]);
-
-                medicines.add(new Medicine(tempName, tempHour, tempMin, tempFreq));
-            }
-        }
-        else
-            medicines.add(new Medicine("Tap me to edit!","00","30",1)); //initial placeholder text to guide user through editing a medicine for first time
-
-        //create and set array adapter for medicines and medSchedule listview
-        adapter = new ArrayAdapter<Medicine>(this, android.R.layout.simple_list_item_1, medicines);
-        medSchedule = (ListView)findViewById(R.id.medSchedule);
-        medSchedule.setAdapter(adapter);
-
-        medSchedule.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id)/*on item click indicates an edit*/ {
-
-                Intent editMedicineActivity = new Intent(MainActivity.this, EditMedicineActivity.class);
-                editMedicineActivity.putExtra("medicineToEdit", medicines.get(position)); //send original medicine values as placeholders for edit activity
-
-                medicines.remove(position); //remove old unedited medicine
-
-                startActivityForResult(editMedicineActivity, REQUEST_CODE);
-            }
-        });
-
-        //create floating action button that adds a new medicine to the medicines list when clicked on
-        FloatingActionButton addMedicine = (FloatingActionButton) findViewById(R.id.addMedicine);
-        addMedicine.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent editMedicineActivity = new Intent(MainActivity.this, EditMedicineActivity.class);
-                editMedicineActivity.putExtra("medicineToEdit", new Medicine("temp", "-1", "-1")); //provide placeholders so we can use EditMedicineActivity instead of creating a redundant new one
-
-                startActivityForResult(editMedicineActivity, REQUEST_CODE);
-            }
-        });
-    }
-
-    //Runs when returning from EditMedicineActivity (after creating or editing a medicine)
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // REQUEST_CODE is defined above
-        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
-
-            //add medicine received from activity (if an edit, we already removed the original one)
-            Medicine newMedicine = (Medicine)data.getSerializableExtra("editedMedicine");
-            medicines.add(newMedicine);
-            adapter.notifyDataSetChanged(); //update listview on change
-
-            setAlarms();
-
-            //create or update file 'medicinesFile.txt'
-            String filename = "medicinesFile.txt";
-
-            String fileContents = "";
-
-            //add each medicine from medicines list to fileContents string, separating each attribute with a new line for later iteration
-            for(int i = 0; i<medicines.size(); i++)
-            {
-                Medicine temp = medicines.get(i);
-                fileContents += temp.medicineName + "\n";
-                fileContents += temp.hour + "\n";
-                fileContents += temp.minute + "\n";
-                fileContents += temp.frequency + "\n";
-            }
-
-            FileOutputStream outputStream;
-
-            //try creating a file 'medicinesFile.txt' with content 'fileContents'
+            //try finding file 'medicinesFile.txt' if it exists
             try {
-                outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
-                OutputStreamWriter outputWriter=new OutputStreamWriter(outputStream);
-                outputWriter.write(fileContents);
-                outputWriter.close();
+                fileInputStream = openFileInput("medicinesFile.txt");
+                InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
 
-                //Log.e(TAG, "Saved as..." + fileContents); //uncomment to read file if need be
+                char[] buffer = new char[1024];
+                int charRead;
+
+                //creates string that contains text from 'medicinesFile.txt'
+                while ((charRead = inputStreamReader.read(buffer)) > 0) {
+                    String readString = String.copyValueOf(buffer, 0, charRead);
+                    medicinesFileText += readString;
+                }
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
 
+            String[] medicinesFileTextArray = medicinesFileText.split("\n"); //split text from 'medicinesFile.txt' by line
+
+            medicines = new ArrayList<Medicine>();
+
+            //if medicinesFileTextArray contains a medicine (min length for a medicine is 4); this only triggers if user has not created a medicine yet
+            if (medicinesFileTextArray.length > 2) {
+
+                //iterate line by line to create the arraylist of medicines
+                for (int i = 0; i < medicinesFileTextArray.length; i += 4) {
+                    String tempName = medicinesFileTextArray[i];
+                    String tempHour = medicinesFileTextArray[i + 1];
+                    String tempMin = medicinesFileTextArray[i + 2];
+                    int tempFreq = Integer.parseInt(medicinesFileTextArray[i + 3]);
+
+                    medicines.add(new Medicine(tempName, tempHour, tempMin, tempFreq));
+                }
+            } else
+                medicines.add(new Medicine("Tap me to edit!", "00", "30", 1)); //initial placeholder text to guide user through editing a medicine for first time
+
+            //create and set array adapter for medicines and medSchedule listview
+            adapter = new ArrayAdapter<Medicine>(this, android.R.layout.simple_list_item_1, medicines);
+            medSchedule = (ListView) findViewById(R.id.medSchedule);
+            medSchedule.setAdapter(adapter);
+
+            medSchedule.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id)/*on item click indicates an edit*/ {
+
+                    Intent editMedicineActivity = new Intent(MainActivity.this, EditMedicineActivity.class);
+                    editMedicineActivity.putExtra("medicineToEdit", medicines.get(position)); //send original medicine values as placeholders for edit activity
+
+                    medicines.remove(position); //remove old unedited medicine
+
+                    startActivityForResult(editMedicineActivity, REQUEST_CODE);
+                }
+            });
+
+            //create floating action button that adds a new medicine to the medicines list when clicked on
+            FloatingActionButton addMedicine = (FloatingActionButton) findViewById(R.id.addMedicine);
+            addMedicine.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent editMedicineActivity = new Intent(MainActivity.this, EditMedicineActivity.class);
+                    editMedicineActivity.putExtra("medicineToEdit", new Medicine("temp", "-1", "-1")); //provide placeholders so we can use EditMedicineActivity instead of creating a redundant new one
+
+                    startActivityForResult(editMedicineActivity, REQUEST_CODE);
+                }
+            });
         }
     }
 
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-    }
+        //Runs when returning from EditMedicineActivity (after creating or editing a medicine)
+        @Override
+        protected void onActivityResult ( int requestCode, int resultCode, Intent data){
+            // REQUEST_CODE is defined above
+            if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+
+                //add medicine received from activity (if an edit, we already removed the original one)
+                Medicine newMedicine = (Medicine) data.getSerializableExtra("editedMedicine");
+                medicines.add(newMedicine);
+                adapter.notifyDataSetChanged(); //update listview on change
+
+                setAlarms();
+
+                //create or update file 'medicinesFile.txt'
+                String filename = "medicinesFile.txt";
+
+                String fileContents = "";
+
+                //add each medicine from medicines list to fileContents string, separating each attribute with a new line for later iteration
+                for (int i = 0; i < medicines.size(); i++) {
+                    Medicine temp = medicines.get(i);
+                    fileContents += temp.medicineName + "\n";
+                    fileContents += temp.hour + "\n";
+                    fileContents += temp.minute + "\n";
+                    fileContents += temp.frequency + "\n";
+                }
+
+                FileOutputStream outputStream;
+
+                //try creating a file 'medicinesFile.txt' with content 'fileContents'
+                try {
+                    outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+                    OutputStreamWriter outputWriter = new OutputStreamWriter(outputStream);
+                    outputWriter.write(fileContents);
+                    outputWriter.close();
+
+                    //Log.e(TAG, "Saved as..." + fileContents); //uncomment to read file if need be
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+
 
     public void setAlarms() {
         Log.e(TAG, "Started creating the alarms.");
