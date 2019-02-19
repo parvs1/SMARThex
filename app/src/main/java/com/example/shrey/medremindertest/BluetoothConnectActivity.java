2594f67dbf13efc5b366b9f7d7f98bd891ad9264
Shreyas Raikhelkar <shreyas.raikhelkar@gmail.com>
2019-02-19 18:56:55 -0500

    -Attempted connecting to Bluetooth devices and failed

--- app/src/main/java/com/example/shrey/medremindertest/BluetoothConnectActivity.java
+++ app/src/main/java/com/example/shrey/medremindertest/BluetoothConnectActivity.java
@@ -5,13 +5,18 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Handler;
 import android.provider.SyncStateContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -19,51 +24,71 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class BluetoothConnectActivity extends AppCompatActivity {
-    private BluetoothAdapter bleadapter;
     public final String TAG = "MEDICATION_ADHERENCE";
+
+    BluetoothManager BluetoothManager;
+    BluetoothAdapter bleAdapter;
     ListView devicesList;
     ArrayList<BluetoothDevice> devices;
     ArrayAdapter<BluetoothDevice> deviceAdapter;
-    private final ScanCallback scanCallback = new ScanCallback()
-    {
-        @Override
-        public void onScanResult(int callbackType, ScanResult result) {
-            devices.add(result.getDevice());
-            deviceAdapter.notifyDataSetChanged();
-        }
-    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bluetooth_connect);
-        devicesList = (ListView)findViewById(R.id.deviceList);
+        setContentView(R.layout.bluetooth_connect);
+        devicesList = (ListView)findViewById(R.id.devicesList);
         devices = new ArrayList<BluetoothDevice>();
-        devicesList.setAdapter(deviceAdapter);
+
         deviceAdapter = new ArrayAdapter<BluetoothDevice>(this, android.R.layout.simple_list_item_1,devices);
+        devicesList.setAdapter(deviceAdapter);
 
-
-
-        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
-            Toast.makeText(this, "Bluetooth not available", Toast.LENGTH_SHORT).show();
-        }
-
-        final BluetoothManager bleManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-        bleadapter = bleManager.getAdapter();
-    }
-
-    private void runLeScan() {
-        // TODO ScanFilter https://developer.android.com/reference/android/bluetooth/le/ScanFilter
-        bleadapter.getBluetoothLeScanner().startScan(scanCallback);
-        new Handler().postDelayed(new Runnable() {
+        devicesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void run() {
-                bleadapter.getBluetoothLeScanner().stopScan(scanCallback);
-                Log.d(TAG, "Stopped Scanner");
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                BluetoothDevice temp = devices.get(position);
+
 
             }
-        }, 10000);
+        });
+
+        // Register for broadcasts when a device is discovered.
+
+        BluetoothManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+        bleAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (!bleAdapter.isDiscovering())
+            bleAdapter.startDiscovery();
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver, filter);
     }
 
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            Log.e(TAG, "Found a device");
+
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                if(device.getType() == BluetoothDevice.DEVICE_TYPE_LE) {
+                    Log.e(TAG,"Found LE Device!");
+                    devices.add(device);
+                    deviceAdapter.notifyDataSetChanged();
+                }
+            }
+        }
+    };
+
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(receiver);
+    }
 
 }
