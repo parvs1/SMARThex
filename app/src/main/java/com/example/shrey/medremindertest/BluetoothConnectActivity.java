99c1488467d0d1244bd3015e47b340f1cadee52c
Parv Shrivastava <parvshrivastava@gmail.com>
2019-02-20 08:04:28 -0500

    put mintu back in

--- app/src/main/java/com/example/shrey/medremindertest/BluetoothConnectActivity.java
+++ app/src/main/java/com/example/shrey/medremindertest/BluetoothConnectActivity.java
@@ -1,6 +1,5 @@
 package com.example.shrey.medremindertest;
 
-import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
@@ -8,109 +7,70 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class BluetoothConnectActivity extends AppCompatActivity {
-    public final String TAG = "MEDICATION_ADHERENCE";
+	public final String TAG = "MEDICATION_ADHERENCE";
 
-    BluetoothManager BluetoothManager;
-    BluetoothAdapter mBluetoothAdapter;
-    ListView devicesList;
-    public ArrayList<BluetoothDevice> devices = new ArrayList<>();
-    public ArrayAdapter<BluetoothDevice> deviceAdapter;
+	BluetoothManager BluetoothManager;
+	BluetoothAdapter bluetoothAdapter;
+	ListView devicesList;
+	ArrayList<BluetoothDevice> devices;
+	ArrayAdapter<BluetoothDevice> deviceAdapter;
 	private static final int REQUEST_ENABLE_BT = 1;
-    private Button discovery;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.bluetooth_connect);
-        devicesList = (ListView)findViewById(R.id.deviceList);
-        deviceAdapter = new DeviceListAdapter(this, android.R.layout.simple_list_item_1,devices);
-        devicesList.setAdapter(deviceAdapter);
-        Log.d(TAG, "btnDiscover: Looking for unpaired devices.");
+	private MintuReceiver mintuReceiver;
 
-        if(mBluetoothAdapter.isDiscovering()){
-            mBluetoothAdapter.cancelDiscovery();
-            Log.d(TAG, "btnDiscover: Canceling discovery.");
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.bluetooth_connect);
+		devicesList = (ListView)findViewById(R.id.devicesList);
+		devices = new ArrayList<BluetoothDevice>();
 
-            //check BT permissions in manifest
-            checkBTPermissions();
+		deviceAdapter = new ArrayAdapter<BluetoothDevice>(this, android.R.layout.simple_list_item_1,devices);
+		devicesList.setAdapter(deviceAdapter);
 
-            mBluetoothAdapter.startDiscovery();
-            IntentFilter discoverDevicesIntent = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-            registerReceiver(receiver, discoverDevicesIntent);
-        }
-        if(!mBluetoothAdapter.isDiscovering()){
+		mintuReceiver = new MintuReceiver();
 
-            //check BT permissions in manifest
-            checkBTPermissions();
-
-            mBluetoothAdapter.startDiscovery();
-            IntentFilter discoverDevicesIntent = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-            registerReceiver(receiver, discoverDevicesIntent);
-        }
-/*
-        devicesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                BluetoothDevice temp = devices.get(position);
+		devicesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				BluetoothDevice temp = devices.get(position);
 
 
-            }
-        });
-        */
-    }
+			}
+		});
 
+		// Register for broadcasts when a device is discovered.
+		//BluetoothManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+		final BluetoothManager bluetoothManager =
+				(BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+		bluetoothAdapter = bluetoothManager.getAdapter();
 
-    // Create a BroadcastReceiver for ACTION_FOUND.
-    private final BroadcastReceiver receiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            Log.e(TAG, "Found a device");
+		if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+			Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		}
+		if (!bluetoothAdapter.isDiscovering())
+			bluetoothAdapter.startDiscovery();
 
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Discovery has found a device. Get the BluetoothDevice
-                // object and its info from the Intent.
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+		IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+		BluetoothConnectActivity.this.registerReceiver(mintuReceiver, filter);
+	}
 
-                if(device.getType() == BluetoothDevice.DEVICE_TYPE_LE) {
-                    Log.e(TAG,"Found LE Device!");
-                    devices.add(device);
-                    deviceAdapter = new DeviceListAdapter(context,R.layout.activity_device_list_adapter,devices);
-                    deviceAdapter.notifyDataSetChanged();
-                    devicesList.setAdapter(deviceAdapter);
-                }
-            }
-        }
-    };
-    private void checkBTPermissions() {
-        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
-            int permissionCheck = this.checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION");
-            permissionCheck += this.checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION");
-            if (permissionCheck != 0) {
+	protected void onDestroy() {
+		super.onDestroy();
 
-                this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001); //Any number
-            }
-        }else{
-            Log.d(TAG, "checkBTPermissions: No need to check permissions. SDK version < LOLLIPOP.");
-        }
-    }
-
-    protected void onDestroy() {
-        super.onDestroy();
-
-        // Don't forget to unregister the ACTION_FOUND receiver.
-        //unregisterReceiver(receiver);
-    }
+		// Don't forget to unregister the ACTION_FOUND receiver.
+		unregisterReceiver(receiver);
+	}
 
 }
