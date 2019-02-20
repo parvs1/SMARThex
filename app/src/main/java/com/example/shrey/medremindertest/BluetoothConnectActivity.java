f6af27e2171cfec673c098ded5e0febd7b776986
pdant <pdantu1234@gmail.com>
2019-02-20 08:33:53 -0500

    Tried to fix Connection - Added permissions to Mintu

--- app/src/main/java/com/example/shrey/medremindertest/BluetoothConnectActivity.java
+++ app/src/main/java/com/example/shrey/medremindertest/BluetoothConnectActivity.java
@@ -1,5 +1,6 @@
 package com.example.shrey.medremindertest;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
@@ -7,6 +8,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,59 +20,72 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class BluetoothConnectActivity extends AppCompatActivity {
-	public final String TAG = "MEDICATION_ADHERENCE";
+    public final String TAG = "MEDICATION_ADHERENCE";
 
-	BluetoothManager BluetoothManager;
-	BluetoothAdapter bluetoothAdapter;
-	ListView devicesList;
-	ArrayList<BluetoothDevice> devices;
-	ArrayAdapter<BluetoothDevice> deviceAdapter;
-	private static final int REQUEST_ENABLE_BT = 1;
-	private MintuReceiver mintuReceiver;
+    BluetoothManager BluetoothManager;
+    BluetoothAdapter bluetoothAdapter;
+    ListView devicesList;
+    ArrayList<BluetoothDevice> devices;
+    ArrayAdapter<BluetoothDevice> deviceAdapter;
+    private static final int REQUEST_ENABLE_BT = 1;
+    private MintuReceiver mintuReceiver;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.bluetooth_connect);
-		devicesList = (ListView)findViewById(R.id.devicesList);
-		devices = new ArrayList<BluetoothDevice>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bluetooth_connect);
+        devicesList = (ListView)findViewById(R.id.devicesList);
+        devices = new ArrayList<BluetoothDevice>();
 
-		deviceAdapter = new ArrayAdapter<BluetoothDevice>(this, android.R.layout.simple_list_item_1,devices);
-		devicesList.setAdapter(deviceAdapter);
+        deviceAdapter = new ArrayAdapter<BluetoothDevice>(this, android.R.layout.simple_list_item_1,devices);
+        devicesList.setAdapter(deviceAdapter);
 
-		mintuReceiver = new MintuReceiver();
+        mintuReceiver = new MintuReceiver();
 
-		devicesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				BluetoothDevice temp = devices.get(position);
+        devicesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                BluetoothDevice temp = devices.get(position);
 
 
-			}
-		});
+            }
+        });
 
-		// Register for broadcasts when a device is discovered.
-		//BluetoothManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
-		final BluetoothManager bluetoothManager =
-				(BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-		bluetoothAdapter = bluetoothManager.getAdapter();
+        // Register for broadcasts when a device is discovered.
+        //BluetoothManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
 
-		if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
-			Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-		}
-		if (!bluetoothAdapter.isDiscovering())
-			bluetoothAdapter.startDiscovery();
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+        if (!bluetoothAdapter.isDiscovering())
+            bluetoothAdapter.startDiscovery();
+            checkBTPermissions();
 
-		IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-		BluetoothConnectActivity.this.registerReceiver(mintuReceiver, filter);
-	}
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        BluetoothConnectActivity.this.registerReceiver(mintuReceiver, filter);
+    }
+    private void checkBTPermissions() {
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
+            int permissionCheck = this.checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION");
+            permissionCheck += this.checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION");
+            if (permissionCheck != 0) {
 
-	protected void onDestroy() {
-		super.onDestroy();
+                this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001); //Any number
+            }
+        }else{
+            Log.d(TAG, "checkBTPermissions: No need to check permissions. SDK version < LOLLIPOP.");
+        }
+    }
 
-		// Don't forget to unregister the ACTION_FOUND receiver.
-		unregisterReceiver(mintuReceiver);
-	}
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(mintuReceiver);
+    }
 
 }
--- app/src/main/java/com/example/shrey/medremindertest/MintuReceiver.java
+++ app/src/main/java/com/example/shrey/medremindertest/MintuReceiver.java
@@ -24,4 +24,5 @@ public class MintuReceiver extends BroadcastReceiver {
             }
         }
     }
+
 }
