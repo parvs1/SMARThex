a6ef25316d6698f295eb17309b431152dc41bb97
pdant <pdantu1234@gmail.com>
2019-05-01 20:34:19 -0400

    Added Home Screen.

--- app/src/main/java/com/example/medication_app/HomeScreen.java
+++ app/src/main/java/com/example/medication_app/HomeScreen.java
@@ -1,19 +1,54 @@
 package com.example.medication_app;
 
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity {
+    Button editSchedule;
+    Button settings;
+    Button dashboard;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-        Button editSchedule;
-        Button settings;
-        Butt
+
+        editSchedule = (Button) findViewById(R.id.editMedSchedule);
+        editSchedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomeScreen.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+    settings = (Button) findViewById(R.id.settings);
+    settings.setOnClickListener(new View.OnClickListener()
+
+    {
+        @Override
+        public void onClick (View v){
+        //Intent i = new Intent(HomeScreen.this, SettingsActivity.class);
+        //startActivity(i);
 
     }
+    });
+
+    dashboard = (Button) findViewById(R.id.dashboard);
+
+    dashboard.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View v){
+                Intent i = new Intent(HomeScreen.this, Dashboard.class);
+                startActivity(i);
+
+            }
+        });
+    }
+
 }
