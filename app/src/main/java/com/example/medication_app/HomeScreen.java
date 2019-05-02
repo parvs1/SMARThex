3306fc20ff511353f4f3e1ce11841441947856d6
pdant <pdantu1234@gmail.com>
2019-05-01 20:34:07 -0400

    Merge branch 'master' of C:\Users\pdant\AndroidStudioProjects\medication-adherence-app with conflicts.

---
+++ app/src/main/java/com/example/medication_app/HomeScreen.java
@@ -0,0 +1,19 @@
+package com.example.medication_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        Button editSchedule;
+        Button settings;
+        Butt
+
+    }
+}
